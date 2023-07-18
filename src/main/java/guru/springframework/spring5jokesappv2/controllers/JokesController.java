89947cd5d23650d09524045dfcf751215ee76dd9
Johannes Schwenk <external.johannes.schwenk@kuehne-nagel.com>
2023-07-18 15:04:52 +0200

    Add JokesController. Closes #4

---
+++ src/main/java/guru/springframework/spring5jokesappv2/controllers/JokesController.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring5jokesappv2.controllers;
+
+import guru.springframework.spring5jokesappv2.services.RandomJokesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    private final RandomJokesService randomJokesService;
+
+    public JokesController(RandomJokesService randomJokesService) {
+        this.randomJokesService = randomJokesService;
+    }
+
+    @RequestMapping("/")
+    public final String getRandomJoke(Model model) {
+        model.addAttribute("joke", randomJokesService.getRandomJoke());
+        return "index";
+    }
+}
